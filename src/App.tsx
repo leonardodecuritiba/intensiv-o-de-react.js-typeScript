852367025378cc82cd5d20de566e2fd191249e28
sujeitoculto <silva.zanin@gmail.com>
2022-06-26 18:14:20 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import { Description } from './components/Description';
 import { Title } from './components/Title';
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <Title title="Live de React do Full Cycle" />
       <Description color="cyan" variant="sm">
         Live de React do Full Cycle
       </Description>
+      <p>Contador de cliques: {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Me clique
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Zera click
+      </button>
     </div>
   );
 }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,3 @@
 export function Title(props: { title: string }) {
-  console.log(props);
   return <h1 style={{ color: 'red' }}>{props.title}</h1>;
 }
